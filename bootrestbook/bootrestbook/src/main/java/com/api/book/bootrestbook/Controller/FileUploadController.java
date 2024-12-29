1fb3277cad4731cb14d27ed45118a89224a179fa
Sanidhya Gupta <guptasanidhya6@gmail.com>
2024-12-29 11:36:17 +0530

    adding image upload feature

---
+++ bootrestbook/bootrestbook/src/main/java/com/api/book/bootrestbook/Controller/FileUploadController.java
@@ -0,0 +1,47 @@
+package com.api.book.bootrestbook.Controller;
+
+import com.api.book.bootrestbook.Helper.FileUploadHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Objects;
+
+@RestController
+public class FileUploadController {
+
+    @Autowired
+    FileUploadHelper fileUploadHelper;
+
+    @PostMapping("/upload-file")
+    public ResponseEntity<String> uploadFile(@RequestParam("file")MultipartFile file){
+//        System.out.println(file.getOriginalFilename());
+//        System.out.println(file.getSize());
+//        System.out.println(file.getContentType());
+try {
+    if (file.isEmpty()) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Please select a file to upload");
+    }
+    if (!Objects.equals(file.getContentType(), "image/png")) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("please upload png file");
+    }
+
+    //file upload code
+    boolean f = fileUploadHelper.uploadFile(file);
+
+    if(f){
+        return ResponseEntity.ok("File is successfully uplaoded");
+    }
+
+
+}catch(Exception e){
+    System.out.println(e);
+}
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong. try again!");
+    }
+}
---
+++ bootrestbook/bootrestbook/src/main/java/com/api/book/bootrestbook/Helper/FileUploadHelper.java
@@ -0,0 +1,46 @@
+package com.api.book.bootrestbook.Helper;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Component
+public class FileUploadHelper {
+public final String UPLOAD_DIR="";
+
+ public boolean uploadFile(MultipartFile multipartFile){
+     boolean f=false;
+
+     try{
+//         InputStream is = multipartFile.getInputStream();
+//         byte data[] = new byte[is.available()];
+//         is.read(data);
+//
+//         FileOutputStream fos = new FileOutputStream(UPLOAD_DIR+ File.separator+ multipartFile.getOriginalFilename());
+//         fos.write(data);
+//
+//         fos.flush();
+//         fos.close();
+//         f = true;
+
+
+         Files.copy(multipartFile.getInputStream(), Paths.get(UPLOAD_DIR+ File.separator+ multipartFile.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
+        f=true;
+     }
+     catch(Exception e ){
+         System.out.println(e);
+     }
+
+
+     //we can use nio package for one liner
+
+        return f;
+ }
+}
